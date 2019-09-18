f2e533ca8066545e8c587a7c6c055cf16ca1c5a8
Manuele Onofri <manuele.onofri@gmail.com>
2019-09-18 14:34:59 +0200

    Complete new Index page

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
-
+ 
 import Layout from '../components/Layout'
-import Features from '../components/Features'
-import BlogRoll from '../components/BlogRoll'
+import PortfolioFeatures from '../components/PortfolioFeatures'
+import BlogRollFeatures from '../components/BlogRollFeatures'
 
 export const IndexPageTemplate = ({
   image,
@@ -16,101 +16,69 @@ export const IndexPageTemplate = ({
   intro,
 }) => (
   <div>
-    <div 
-      className="full-width-image margin-top-0"
-      style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-        backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`,
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          height: '150px',
-          lineHeight: '1',
-          justifyContent: 'space-around',
-          alignItems: 'left',
-          flexDirection: 'column',
-        }}
-      >
-        <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {title}
-        </h1>
-        <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {subheading}
-        </h3>
-      </div>
-    </div>
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="section">
-          <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <div className="content">
-                <div className="content">
-                  <div className="tile">
-                    <h1 className="title">{mainpitch.title}</h1>
-                  </div>
-                  <div className="tile">
-                    <h3 className="subtitle">{mainpitch.description}</h3>
-                  </div>
-                </div>
-                <div className="columns">
-                  <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">
-                      {heading}
-                    </h3>
-                    <p>{description}</p>
-                  </div>
-                </div>
-                <Features gridItems={intro.blurbs} />
-                <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
-                  </div>
-                </div>
-                <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
-                  </h3>
-                  <BlogRoll />
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+	<section 
+		id="banner"
+		style={{
+			backgroundImage: `url(${
+			  !!image.childImageSharp ? image.childImageSharp.fluid.src : image
+			})`,
+			backgroundPosition: `top center`,
+			backgroundAttachment: `fixed`,
+			backgroundSize: 'cover'
+		}}
+	>
+		<div className="inner">
+
+			<header>
+				<h2>{mainpitch.title}</h2>
+			</header>
+			<p>{mainpitch.description}</p>
+			<footer>
+				<ul className="buttons stacked">
+					<li><Link to="#main" className="button fit scrolly">Tell Me More</Link></li>
+				</ul>
+			</footer>
+		</div>
+	</section>
+
+		
+	<article id="main">
+
+		<header className="special container">
+			<span className="icon solid fa-chart-bar"></span>
+			<h2>{heading}</h2>
+			<p>{subheading}</p>
+		</header>
+	
+		<section className="wrapper style3 container special">
+
+			<header className="major">
+				<h2><strong>MY PORTFOLIO</strong></h2>
+			</header>
+
+			<PortfolioFeatures gridItems={intro.blurbs} />
+
+			<footer className="major">
+				<ul className="buttons">
+					<li><Link to="#" className="button">See More</Link></li>
+				</ul>
+			</footer>
+
+		</section>
+
+		<section className="wrapper style1 container special">
+			<header className="major">
+				<h2>MY BLOG</h2>
+			</header>
+			<BlogRollFeatures />
+			<footer className="major">
+				<ul className="buttons">
+					<li><Link to="./blog" className="button">See More</Link></li>
+				</ul>
+			</footer>
+		</section>
+
+	</article>
   </div>
 )
 
